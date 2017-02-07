b13ac20197dce34b40d541f1657df8ffdef333aa
Ed Spry <edspry@verizon.net>
2017-02-07 17:27:28 -0600

    initial commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
   // your code will go here
-
+  // https://www.codeschool.com/users/edspry.json
+	$.ajax({
+		url: 'https://www.codeschool.com/users/edspry.json',
+		dataType: 'jsonp',
+		success: function(response) {
+			$.each(response.courses.completed, function(key, value) {
+				$('#badges').append(
+					'<div class="course">' +
+						'<h3>' + value.title + '</h3>' +
+						'<img src="' + value.badge + '">' +
+						'<a href="' + value.url + '" class="btn btn-primary" target="_blank">See Course</a>' +
+					'</div>'
+				);
+			});
+		}
+	});
 });
